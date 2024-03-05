9eef7bb37a3bd8918e3b0386e079dc0537e4e363
Bharathkumar Tamilarasu <136069598+Bharathkumar-Tamilarasu@users.noreply.github.com>
2024-03-06 01:01:12 +0530

    Edited the print in app.py

--- model/app.py
+++ model/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 from predict_page import show_predict_page
 
-print("Starting the app..... Please visit the given link")
+print("Starting the app..... Please visit the link")
 show_predict_page()
